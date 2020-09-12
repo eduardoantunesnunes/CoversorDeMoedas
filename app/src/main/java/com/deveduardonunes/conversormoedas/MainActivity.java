c8b9506ae955521fc6f265344b89ac15b35c21b8
Eduardo Nunes <eduardoanunes@outlook.com>
2020-09-12 18:00:50 -0300

    Terceiro commit - Termino da implementação da lógica e adição da moeda libras

--- app/src/main/java/com/deveduardonunes/conversormoedas/MainActivity.java
+++ app/src/main/java/com/deveduardonunes/conversormoedas/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewHolder.editValue = findViewById(R.id.edit_value);
         this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.textLibras = findViewById(R.id.text_libras);
+
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
         this.mViewHolder.buttonCalculate.setOnClickListener(this);
 
@@ -37,6 +39,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //Mostra mensagem pro usuário
                 Toast.makeText(this, R.string.informe_valor, Toast.LENGTH_LONG).show();
             }else{
+                //Lógica para converter o valor de real para euro, dolar e libras, respectivamente
+                Double real = Double.valueOf(value); // Transformando o valor em double
+
+                this.mViewHolder.textEuro.setText(String.format("%.2f",(real/6.30))); // Convertendo para Euro
+                this.mViewHolder.textDolar.setText(String.format("%.2f",(real/5.32))); // Convertendo para Dolar
+                this.mViewHolder.textLibras.setText(String.format("%.2f",(real/6.80))); // Covertendo para Libras
+
 
             }
         }
@@ -44,11 +53,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void clearValues(){
         this.mViewHolder.textDolar.setText("");
         this.mViewHolder.textEuro.setText("");
+        this.mViewHolder.textLibras.setText("");
+
     }
     private static class ViewHolder{
         EditText editValue;
         TextView textDolar;
         TextView textEuro;
+        TextView textLibras;
         Button buttonCalculate;
     }
 
