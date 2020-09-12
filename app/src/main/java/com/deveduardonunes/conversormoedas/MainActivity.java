5fa56c403c3088de23885804a17ce04494876a35
Eduardo Nunes <eduardoanunes@outlook.com>
2020-09-12 16:48:09 -0300

    Segundo commit

--- app/src/main/java/com/deveduardonunes/conversormoedas/MainActivity.java
+++ app/src/main/java/com/deveduardonunes/conversormoedas/MainActivity.java
@@ -3,12 +3,53 @@ package com.deveduardonunes.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View Holders para achar identificar os IDs
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.editValue.getText().toString();
+            if("".equals(value)){
+                //Mostra mensagem pro usuário
+                Toast.makeText(this, R.string.informe_valor, Toast.LENGTH_LONG).show();
+            }else{
+
+            }
+        }
+    }
+    private void clearValues(){
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
+    }
+
 }
